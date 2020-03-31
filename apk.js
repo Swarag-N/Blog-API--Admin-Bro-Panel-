ce2202db2db37950bd80b830b22d999e3102b09b
whois-this <46181010+whois-this@users.noreply.github.com>
2020-03-31 23:40:42 +0530

    Admin Bro Implemented.

--- apk.js
+++ apk.js
@@ -1,24 +1,48 @@
-const express 	= require("express"),
-	mongoose 	= require("mongoose"),
-	Blog 		= require("./models/blogs.js"),
+const express = require("express"),
+	mongoose = require("mongoose"),
 	session = require('express-session'),
 	expressSanitizer = require("express-sanitizer"),
-	adminRouter=require("./router/adminbro"),
+	adminRouter = require("./router/adminbro"),
 	indexRouter = require('./router/indexRoutes'),
-	bodyParser=require("body-parser"),
-	apk 		= express();
+	bodyParser = require("body-parser"),
+	apk = express();
 
-apk.use(expressSanitizer());
-apk.use(bodyParser.json());
-const MongoDataBase = process.env.MONGO_URl ||"mongodb://localhost:27017/home";
-const connectDb = async ()=>{
+
+const MongoDataBase = process.env.MONGO_URl || "mongodb://localhost:27017/home";
+const connectDb = async () => {
 	mongoose.connect(MongoDataBase, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	}, ()=>{
-		apk.use("/admin",adminRouter);
-		console.warn("db connected");
-	}
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}, () => {
+			apk.use("/admin", adminRouter);
+			apk.use(expressSanitizer());
+			apk.use(bodyParser.json());
+			apk.use("/", indexRouter);
+			apk.use(session({
+				secret: 'keyboard cat',
+				name: "C4Projects",
+				resave: true,
+				saveUninitialized: true,
+				cookie: {secure: true}
+			}));
+			console.log("Db Connected");
+		}
+		// apk.use("/admin",adminRouter);
+		// apk.use(expressSanitizer());
+		// apk.use(bodyParser.json());
+		// console.warn("db connected");
+		//
+		// 	apk.use("/",indexRouter);
+		//
+		// 	apk.use(session({
+		// 		secret: 'keyboard cat',
+		// 		name:"C4Projects",
+		// 		resave: true,
+		// 		saveUninitialized: true,
+		// 		cookie: { secure: true }
+		// 	}));
+
+		// }
 	);
 };
 connectDb();
@@ -28,21 +52,12 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
 
-apk.use("/",indexRouter);
-
-apk.use(session({
-	secret: 'keyboard cat',
-	name:"C4Projects",
-	resave: true,
-	saveUninitialized: true,
-	cookie: { secure: true }
-}));
-
-
-const run = async()=>{
+const run = async () => {
 	// await
 	// await mongoose.connect(MongoDataBase, { useNewUrlParser: true});
-	await apk.listen(3000,()=>{console.log("Server Is Running")});
+	await apk.listen(3000, () => {
+		console.log("Server Is Running")
+	});
 };
 
 run();
--- router/adminbro.js
+++ router/adminbro.js
@@ -1,55 +1,54 @@
 const AdminBro = require('admin-bro'),
-    express=require('express'),
+    express = require('express'),
     AdminBroExpress = require('admin-bro-expressjs'),
     AdminBroMongoose = require('admin-bro-mongoose'),
-    Blog=require('../models/blogs'),
-    session = require('express-session');
+    Blog = require('../models/blogs');
 
 AdminBro.registerAdapter(AdminBroMongoose);
 
-// const adminRouter = express.Router();
+const adminRouter = express.Router();
 
 const adminBro = new AdminBro({
-  resources:[{
-      resource: Blog,
-      options:{
-          listProperties:['title','date'],
-          filterProperties:["date","title"],
-          editProperties :["title", "info", "image"]
-      },
-  }],
-  softwareBrothers: false,
-	branding:{
-    companyName: 'C4P'
+        resources: [{
+            resource: Blog,
+            options: {
+                listProperties: ['title', 'date'],
+                filterProperties: ["date", "title"],
+                editProperties: ["title", "info", "image"]
+            },
+        }],
+        softwareBrothers: false,
+        branding: {
+            companyName: 'C4P'
+        }
     }
-  }
 );
 
-const router = AdminBroExpress.buildRouter(adminBro);
-module.exports = router;
-// const Admin ={
-// 	email : process.env.EMAIL_OF_ADMIN   || 'example@c4p.com',
-// 	password : process.env.PASSWORD_OF_ADMIN || 'coding'
-// };
-//
-//
-// AdminBroExpress.buildAuthenticatedRouter(
-//     adminBro,
-//     {
-//         authenticate:async(email, password)=>{
-//             if (email === Admin.email && password === Admin.password) {
-//                 return Admin
-//             }
-//             return null
-//         },
-//         cookieName: process.env.COOKIE_ADMIN_NAME || 'C-4-P',
-//         cookiePassword: process.env.COOKIE_ADMIN_PASSWORD || 'admin-bro',
-//     },
-//     adminRouter,
-//     {
-//         resave:true,
-//         saveUninitialized: true
-//     }
-//     );
+// const router = AdminBroExpress.buildRouter(adminBro);
+// module.exports = router;
+const Admin = {
+    email: process.env.EMAIL_OF_ADMIN || 'example@c4p.com',
+    password: process.env.PASSWORD_OF_ADMIN || 'coding'
+};
 
-// module.exports = adminRouter;
+
+AdminBroExpress.buildAuthenticatedRouter(
+    adminBro,
+    {
+        authenticate: async (email, password) => {
+            if (email === Admin.email && password === Admin.password) {
+                return Admin
+            }
+            return null
+        },
+        cookieName: process.env.COOKIE_ADMIN_NAME || 'C-4-P',
+        cookiePassword: process.env.COOKIE_ADMIN_PASSWORD || 'admin-bro',
+    },
+    adminRouter,
+    {
+        resave: true,
+        saveUninitialized: true
+    }
+);
+
+module.exports = adminRouter;
